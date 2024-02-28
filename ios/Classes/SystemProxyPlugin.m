b7dd2763eee2424349fde146725b6f1a0785696e
Andrew Natoli <367870+Kalgar@users.noreply.github.com>
2024-02-28 11:04:59 +1100

    Replicate iOS changes from fachiraful to try fix CI/CD

--- ios/Classes/SystemProxyPlugin.m
+++ ios/Classes/SystemProxyPlugin.m
@@ -1,5 +1,9 @@
 #import "SystemProxyPlugin.h"
+#if __has_include(<system.proxy.retriever/system.proxy.retriever-Swift.h>)
 #import <system.proxy.retriever/system.proxy.retriever-Swift.h>
+#else
+#import "system.proxy.retriever-Swift.h"
+#endif
 
 @implementation SystemProxyPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
